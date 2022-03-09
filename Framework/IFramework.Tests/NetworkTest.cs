d555c619c051a3f28f272309eb2205d9b47257de
psygames <799329256@qq.com>
2022-03-09 14:33:25 +0800

    [test] Create Unit Test Proj & Serialization Unit Test

---
+++ Framework/IFramework.Tests/NetworkTest.cs
@@ -0,0 +1,18 @@
+using IFramework.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace IFramework.Tests
+{
+    // TODO: 网络测试，稍后补齐
+    [TestClass]
+    public class NetworkTest
+    {
+        [TestMethod]
+        public void TcpClient()
+        {
+        }
+    }
+}
---
+++ Framework/IFramework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IFramework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IFramework.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f2544d4b-0aad-4209-a1ae-bf37a9b91076")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
